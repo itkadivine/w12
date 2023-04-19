4c0cfbef3ec62b7cf2e7266cc36a6ad844aed71c
itkadivine <itgelsaikhan@stda.edu.mn>
2023-04-19 17:56:13 +0800

    moved

---
+++ src/w12/Hi.java
@@ -0,0 +1,15 @@
+package w12_1;
+
+public class Hi {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("WORLD");
+		System.out.println("gh");
+		System.out.println("gh");
+//dfgdfg
+//		fgkjfsdgkl
+
+	}
+
+}
